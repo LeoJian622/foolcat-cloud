11a580fd41583a1371861523ef28c7de6f276e62
Richard <hualuoyu_yks@qq.com>
2021-05-26 17:31:03 +0800

    gateway

---
+++ foolcat-iaas/gateway-server/src/main/java/xyz/foolcat/GatewayServerApplication.java
@@ -0,0 +1,20 @@
+package xyz.foolcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Leojan
+ * @date 2021-05-26 16:23
+ */
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
+}
